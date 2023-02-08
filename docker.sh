fa7273b8fefb44a1703ae14e7ed3239c0cabc53a
SJA <adodey1@yahoo.com>
2023-02-08 12:14:49 +0000

    testing ansible install

--- docker.sh
+++ docker.sh
@@ -1,4 +1,6 @@
 #!bin/bash
+set -e
+
 
 apt-get install \
     ca-certificates \
@@ -11,8 +13,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt update
 chmod a+r /etc/apt/keyrings/docker.gpg
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
