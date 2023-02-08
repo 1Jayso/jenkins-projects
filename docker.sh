8d46afc2a87f31a3bef6915e8a31974dec430a0c
SJA <adodey1@yahoo.com>
2023-02-08 12:30:00 +0000

    testing ansible install

--- docker.sh
+++ docker.sh
@@ -15,7 +15,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 chmod a+r /etc/apt/keyrings/docker.gpg
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 apt install python3-pip -y && pip3 install --upgrade pip
 pip install ansible 
 apt install openssh-server openssh-client -y && apt install sshpass -y 
